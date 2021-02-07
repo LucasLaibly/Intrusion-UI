a1a73a16f1cd9586d463b685e7d7108a5816eca4
LucasLaibly <original7245@gmail.com>
2021-02-07 16:27:22 -0600

    overhaul join lobby

--- pages/lobby/join.js
+++ pages/lobby/join.js
@@ -1,24 +1,52 @@
 import Link from 'next/link';
 import Head from 'next/head';
+import * as Mui from '@material-ui/core';
 import Lobby from '../../components/lobby';
 
 // colors : #111d5e(dark blue) #c70039(pink) #f37121(orange) #c0e218 (yellow) -> palette 226115
-export default function JoinLobby() {
+const JoinLobby = () => {
   return (
     <Lobby>
       <div className="container color-code">
         <form>
+
           <div className="form-group">
-            <label htmlFor="uname">Userame</label>
-            <input type="text" id="username" name="username" placeholder="Desired username.." />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lname">Lobby Number</label>
-            <input type="number" id="lobbynumber" name="lobbynumber" placeholder="Lobby Number" />
+            <Mui.TextField 
+              required
+              id="filled-required" 
+              label="Username Required"
+              defaultValue=""
+              variant="outlined" 
+            />
           </div>
 
-          <input type="submit" value="Create Lobby" />
+          <div className="form-group-secondary">
+            <Mui.TextField 
+              required
+              id="filled-required" 
+              label="Lobby Name Required"
+              defaultValue=""
+              variant="outlined"
+            />
+          </div>
+
+          <div className="form-group-secondary">
+            <Mui.TextField 
+              required
+              id="filled-required" 
+              label="Lobby Number Required"
+              defaultValue=""
+              variant="outlined"
+            />
+          </div>
+
+          <div className="form-group-secondary">
+            <Mui.Button variant="contained">
+              Join Lobby
+            </Mui.Button>
+          </div>
         </form>
+
       </div>
 
       <style jsx>
@@ -27,8 +55,28 @@ export default function JoinLobby() {
           background: #c70039;
           min-height: 100vh;
         }
+        .container {
+          flex-direction: column;
+        }
+        .form-group {
+          flex-direction: column;
+        }
+
+        .form-group-secondary {
+          margin-top: 20px;
+        }
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+          margin-top: 3rem;
+        }
       `}
       </style>
     </Lobby>
   );
 }
+
+export default JoinLobby;
